e97a7f670ac9abe40063ada0f2690b8db3aa004d
J040 <umvelhobarbudo@gmail.com>
2025-10-15 17:20:54 +0100

    Adding background image to construction page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
+import styles from '../styles/Home.module.css';
+
 function Home() {
   var content = (
-    <div>
-      <h1>Em construção...</h1>
-    </div>
+    // <div>
+    //   <img src="/resources/background-construction.jpg" />
+    //   {/* <h1>Em construção....</h1> */}
+    // </div>
+    <main className={styles.stage}>
+      <img
+        src="/resources/background-construction.jpg"
+        alt="Em construção"
+        className={styles.fill}
+      />
+    </main>
   );
   return content;
 }
