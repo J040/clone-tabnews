4d0b7ec4e2658ed1c1104a055492396ec4b0f75c
J040 <umvelhobarbudo@gmail.com>
2025-10-21 20:47:09 +0100

    clear

--- infra/database.js
+++ infra/database.js
@@ -2,25 +2,25 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host:process.env.POSTGRES_HOST,
-    port:process.env.POSTGRES_PORT,
-    database:process.env.POSTGRES_DB,
-    user:process.env.POSTGRES_USER, 
-    password:process.env.POSTGRES_PASSWORD,
-    ssl:process.env.NODE_ENV === "production"
-        ? { rejectUnauthorized: false }
-        : false,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER, 
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development"
+        ? false
+        : true,
   });
 
   console.log("Credenciais do Postgres: ", {
-    host:process.env.POSTGRES_HOST,
-    port:process.env.POSTGRES_PORT,
-    database:process.env.POSTGRES_DB,
-    user:process.env.POSTGRES_USER, 
-    password:process.env.POSTGRES_PASSWORD,
-    ssl:process.env.NODE_ENV === "production"
-        ? { rejectUnauthorized: false }
-        : false,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER, 
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development"
+        ? false
+        : true,
   });
 
   try {
