0d5c2369ba6e4877528d1177a3f4aa8abfb484fc
J040 <umvelhobarbudo@gmail.com>
2025-11-02 14:14:07 +0000

    Adding `api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,17 +1,9 @@
 import { Client } from "pg";
 
-async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER, 
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(),
-  });
-
+async function query(queryObject) {  
+  let client;
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject)
     return result;
 
@@ -21,12 +13,25 @@ async function query(queryObject) {
     
   } finally {  
     await client.end();
-
   }
 }
 
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER, 
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+  await client.connect();
+  return client;
+}
+
 export default {
-  query: query,
+  query,
+  getNewClient
 }
 
 function getSSLValues() {
@@ -35,5 +40,6 @@ function getSSLValues() {
       ca: process.env.POSTGRES_CA,
     }
   }
-  return process.env.NODE_ENV === "development" ? false : true;
+
+  return process.env.NODE_ENV === "production" ? true : false;
 }
