4c83aec03b72a91d1c848edc4cead412b6664f18
J040 <umvelhobarbudo@gmail.com>
2025-11-23 20:05:32 +0000

    Fixing files that did not pass the `lint:prittier:check`

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -3,15 +3,14 @@ import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await database.query("drop schema public cascade; create schema public;")
-})
+  await database.query("drop schema public cascade; create schema public;");
+});
 
 test("POST to /api/v1/migrations should return 200", async () => {
-
   // Scope of Response 1 #############
 
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: 'POST'
+    method: "POST",
   });
   expect(response1.status).toBe(201);
 
@@ -23,7 +22,7 @@ test("POST to /api/v1/migrations should return 200", async () => {
   // Scope of Response 2 #############
 
   const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: 'POST'
+    method: "POST",
   });
   expect(response2.status).toBe(200);
 
