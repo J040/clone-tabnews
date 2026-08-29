f02759d779e26777033e70052502f2b4303b88e3
João Victor Ribeiro de Jesus <joaovictor@Joaos-MacBook-Pro.local>
2026-08-29 20:00:41 +0100

    feat: implement session creation

---
+++ infra/migrations/1787174839227_create-sessions.js
@@ -0,0 +1,43 @@
+exports.up = (pgm) => {
+  pgm.createTable("sessions", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+
+    token: {
+      type: "varchar(96)",
+      notNull: true,
+      unique: true,
+    },
+
+    user_id: {
+      type: "uuid",
+      notNull: true,
+    },
+
+    // Why timestamp with timezone? https://justatheory.com/2012/04/postgres-use-timestamptz
+    // timestamptz ~ with "tz" because we wanna have the timezone from where it was created
+    expires_at: {
+      type: "timestamptz",
+      notNull: true,
+    },
+    
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+  });
+};
+
+// "false" diz para o módulo que não existe o ~movimento~ "down" nessa migration.
+// apenas o que está declarado acima, "up".
+exports.down = false;
