74edc50d6cbc4792e1e540ee4c2ea61ac5ffa6ff
João Victor Ribeiro de Jesus <joaovictor@Joaos-MacBook-Pro.local>
2026-09-09 22:51:56 +0100

    feat: implement /api/v1/user endpoint with session renewal

--- models/session.js
+++ models/session.js
@@ -1,7 +1,40 @@
 import crypto from "node:crypto";
 import database from "infra/database.js";
+import { UnauthorizedError } from "infra/errors.js";
 
 const EXPIRATION_IN_MILLISECONDS = 30 * 24 * 60 * 60 * 1000; // 30 days
+
+async function findOneValidByToken(sessionToken) {
+  const sessionFound = await runSelectQuery(sessionToken);
+
+  return sessionFound;
+
+  async function runSelectQuery(sessionToken) {
+    const results = await database.query({
+      text: `
+        SELECT 
+          * 
+        FROM 
+          sessions 
+        WHERE 
+          token = $1
+          AND expires_at > NOW()
+        LIMIT 
+          1
+        ;`,
+      values: [sessionToken],
+    });
+
+    if (results.rowCount == 0) {
+      throw new UnauthorizedError({
+        message: "Usuário não possui sessão ativa.",
+        action: "Verifique se o usuário está logado e tente novamente.",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
 
 async function create(userId) {
   // Por que 48 bytes? Porque queremos um token de 96 caracteres hexadecimais, e cada byte é representado por 2 caracteres hexadecimais.
@@ -30,8 +63,36 @@ async function create(userId) {
   }
 }
 
+async function renew(sessionId) {
+  const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILLISECONDS);
+
+  const renewedSession = await runUpdateQuery(sessionId, expiresAt);
+  return renewedSession;
+
+  async function runUpdateQuery(sessionId, expiresAt) {
+    const results = await database.query({
+      text: `
+        UPDATE 
+          sessions 
+        SET
+          expires_at = $2,
+          updated_at = NOW()
+        WHERE 
+          id = $1
+        RETURNING
+          *
+        ;`,
+      values: [sessionId, expiresAt],
+    });
+
+    return results.rows[0];
+  }
+}
+
 const session = {
   create,
+  findOneValidByToken,
+  renew,
   EXPIRATION_IN_MILLISECONDS,
 };
 
